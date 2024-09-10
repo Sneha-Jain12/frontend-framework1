3fbb1bc579a59b2760831c466af15059d118050f
sneha jain <snehainbab@gmail.com>
2024-09-10 13:28:55 +0200

    Base framework created

---
+++ src/main/java/resouces/staticResources/GlobalStatic.java
@@ -0,0 +1,24 @@
+package resouces.staticResources;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
+public class GlobalStatic {
+
+public static int testcaseId;
+
+ public static WebDriver driver;
+
+ public static Properties prop;
+
+ public static  void generateId(int id){
+ testcaseId=id;
+ }
+
+
+
+
+
+
+}
